c5a2149e45454c2cf60537e2590511b04f5add36
Ricardo Sass <ricardosassofficial@gmail.com>
2023-03-28 15:53:33 +0200

    Finished

--- handle_print.c
+++ handle_print.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * handle_print - Prints an argument based on the type
+ * @fmt: Formatted string in which to print the arguments
+ * @list: List of arguments that will be printed
+ * @ind: ind
+ * @buffer: Buffer array to handle the printing
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: gets width
  * @precision: Precision specification
  * @size: Size specifier
  *
